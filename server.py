947f5c2f4a6eb7e68444a749413c280f0176cf5a
Michael Blaszczyk <michael.i.blaszczyk@gmail.com>
2021-02-06 22:14:24 +0100

    cors header for heizi web

--- server.py
+++ server.py
@@ -68,6 +68,7 @@ class Server(BaseHTTPRequestHandler):
 	def _set_response(self):
 		self.send_response(200)
 		self.send_header('Content-type', 'application/json')
+		self.send_header('Access-Control-Allow-Origin', '*')
 		self.end_headers()
 
 	def do_GET(self):
